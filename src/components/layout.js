2bdc91d5b8fd6aad17f8bfe6396c77774d41a394
binothird <albinoiii.cordova@gmail.com>
2020-12-04 01:46:07 +0800

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 import { Container } from "react-bootstrap";
 
 import Navigation from "./Navigation/Navigation";
-import { CustomerChat } from "./fbmessenger";
 import Footer from "./Footer/Footer";
 import "../style/main.scss";
 
@@ -27,7 +33,6 @@ const Layout = ({ children }) => {
           {children}
         </Container>
       </main>
-      <CustomerChat />
       <Footer />
     </>
   );
