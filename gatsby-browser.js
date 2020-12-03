d45634f9719e7ca3ba6177e41fa8936830e98201
binothird <albinoiii.cordova@gmail.com>
2020-12-04 01:55:04 +0800

    added gatsby api

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require("react");
+const CustomerChat = require("./src/components/fbmessenger").default;
+
+exports.wrapPageElement = ({ element }) => (
+  <>
+    {element}
+    <CustomerChat />
+  </>
+);
