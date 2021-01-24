3f8120e2866db12b940238519fc7ec4c314c895f
binothird <albinoiii.cordova@gmail.com>
2021-01-25 00:12:29 +0800

    remove modal banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from "../components/Seo/Seo";
 import Layout from "../components/layout";
 import MainHeading from "../components/Headings/MainHeading";
 import Icons from "../components/Sections/Icons";
-// import Products from "../components/Products/Products";
+import Products from "../components/Products/Products";
 // import BannerModal from "../components/Modals/BannerModal";
 import BackTop from "../components/Utilities/BackTop";
 
